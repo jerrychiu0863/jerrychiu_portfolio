fac9bdead035c7ae5c9f845c7a8f04155a8ce61b
Jerry <m08632000@hotmail.com>
2018-05-24 12:52:04 -0700

    edited responsive part and added a label

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
            $(".composition__photo--p1").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p2").css({"right": "20px", "opacity": "1"});
            $(".composition__photo--p3").css({"top": "100px", "opacity": "1"});
-       }else if($(this).scrollTop() > 1800 && $(window).width() >= 900 && $(window).width() <= 1200) {
+       }else if($(this).scrollTop() > 2000 && $(window).width() >= 900 && $(window).width() <= 1200) {
            $(".composition__photo--p1").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p2").css({"right": "20px", "opacity": "1"});
            $(".composition__photo--p3").css({"top": "80px", "opacity": "1"});
-       } else if($(this).scrollTop() > 3000 && $(window).width() <= 900) {
+       } else if($(this).scrollTop() > 2800 && $(window).width() <= 900) {
            $(".composition__photo--p1").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p2").css({"right": "0px", "opacity": "1"});
            $(".composition__photo--p3").css({"top": "-25px", "opacity": "1"});
@@ -24,11 +24,11 @@ $(window).scroll(function() {
            $(".composition__photo--p4").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p5").css({"right": "20px", "opacity": "1"});
            $(".composition__photo--p6").css({"top": "100px", "opacity": "1"});
-       }else if($(this).scrollTop() > 2600 && $(window).width() >= 900 && $(window).width() <= 1200) {
+       }else if($(this).scrollTop() > 2500 && $(window).width() >= 900 && $(window).width() <= 1200) {
            $(".composition__photo--p4").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p5").css({"right": "20px", "opacity": "1"});
            $(".composition__photo--p6").css({"top": "80px", "opacity": "1"});
-       } else if($(this).scrollTop() > 3600 && $(window).width() <= 900) {
+       } else if($(this).scrollTop() > 3300 && $(window).width() <= 900) {
            $(".composition__photo--p4").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p5").css({"right": "0px", "opacity": "1"});
            $(".composition__photo--p6").css({"top": "-25px", "opacity": "1"});
@@ -42,11 +42,11 @@ $(window).scroll(function() {
            $(".composition__photo--p7").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p8").css({"right": "20px", "opacity": "1"});
            $(".composition__photo--p9").css({"top": "100px", "opacity": "1"});
-       }else if($(this).scrollTop() > 3000 && $(window).width() >= 900 && $(window).width() <= 1200) {
+       }else if($(this).scrollTop() > 2900 && $(window).width() >= 900 && $(window).width() <= 1200) {
            $(".composition__photo--p7").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p8").css({"right": "20px", "opacity": "1"});
            $(".composition__photo--p9").css({"top": "80px", "opacity": "1"});
-       } else if($(this).scrollTop() > 4000 && $(window).width() <= 900) {
+       } else if($(this).scrollTop() > 3700 && $(window).width() <= 900) {
            $(".composition__photo--p7").css({"top": "-20px","opacity": "1"});
            $(".composition__photo--p8").css({"right": "0px", "opacity": "1"});
            $(".composition__photo--p9").css({"top": "-25px", "opacity": "1"});
